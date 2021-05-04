a8d2f99d78064824152063c169a6a5ec56d9df28
demidraksin <greatraksin@icloud.com>
2021-05-04 18:36:24 +0300

    Adding phone number to db⭐️

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(120), unique=True)
+    phone = db.Column(db.String(12), unique=True, nullable=True)
     password_hash = db.Column(db.String(128))
 
     def __str__(self):
